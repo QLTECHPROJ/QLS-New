c163c3234131b989002fb77e9961c2cf6d8753cf
SuchiMehta26 <suchi2616@gmail.com>
2023-02-17 17:02:03 +0530

    commit 17-2-

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,29 +17,11 @@ import NewSlider from "../components/NewSlider";
 
 const IndexPage = ({ data }) => {
  
-
-  // const [visible, setVisible] = useState(false);
-  // const [visibles, setVisibles] = useState(false);
+  const getAnimationDelay = (index) => {
+    // calculate delay time for each section
+    return index * 500; // adjust delay time as needed
+  };
   
-  // useEffect(() => {
-  //   const element = document.querySelector(".bner_sec_text_d");
-  //   element.addEventListener("animationend", () => {
-  //     setVisible(true);
-  
-  //     setTimeout(() => {
-  //       const elements = document.querySelector(".bnersec_txt2");
-  //       elements.addEventListener("animationend", () => {
-  //         setVisibles(true);
-  //       });
-  //     }, 500); // delay the second animation by 500ms
-  //   });
-  
-  //   return () => {
-  //     element.removeEventListener("animationend", () => {
-  //       setVisible(true);
-  //     });
-  //   };
-  // }, []);
   return (
     <Layout>
       <main>
@@ -105,25 +87,25 @@ const IndexPage = ({ data }) => {
 
               {/* <MyComponent /> */}
 
-<section class="banner_sec" style={{ backgroundImage: `url(../../bnerimg.png)` }}>
+<section className="banner_sec" style={{ backgroundImage: `url(../../bnerimg.png)` }}>
 			
-			<div class="our_banner_top">
-				<div class="container">
-					<div class="row justify-content-center">
-          <ReactWOW  animation='fadeInUp'>
-                        <div class="col-md-12">
-                            <div class="earth_new_mio" >
+			<div className="our_banner_top">
+				<div className="container">
+					<div className="row justify-content-center">
+          <ReactWOW  animation='fadeInUp' duration="2s">
+                        <div className="col-md-12">
+                            <div className="earth_new_mio" >
                                 <img src="../../earth_wrap.png" />
                             </div>
                           
                         </div>
                         </ReactWOW>
-                        <ReactWOW delay='0.4s' animation='fadeInUp'>                          
-						<div class="col-md-12">
+                        <ReactWOW delay='2s' animation='fadeInUp'>                          
+						<div className="col-md-12">
             
-							<div class="bner_sec_text_d"  >
+							<div className="bner_sec_text_d"  >
               
-								<div class="bner_main_tx">
+								<div className="bner_main_tx">
 									
                 <div dangerouslySetInnerHTML={{ __html: data.wpPage.homeSlider.sliderHeading }} />
                 
@@ -133,10 +115,10 @@ const IndexPage = ({ data }) => {
               
 						</div>
             </ReactWOW>
-						<div class="col-md-12">
-							<div class="bnersec_txt2 bn_cvb">
-              <ReactWOW delay='2s' animation='fadeInUp'> 
-								<div class="p_txt">
+						<div className="col-md-12">
+							<div className="bnersec_txt2 bn_cvb">
+              <ReactWOW delay='5s' animation='fadeInUp'> 
+								<div className="p_txt">
                 <div dangerouslySetInnerHTML={{ __html: data.wpPage.homeSlider.sliderSubheading }} />
 								</div>
                </ReactWOW>
@@ -148,12 +130,12 @@ const IndexPage = ({ data }) => {
 			</div>
 		</section>
 
-    <section class="about_us_sec pb_100 " style={{ backgroundImage: `url(../../Banne.png)` }}>
-			<div class="container">
-				<div class="row align-items-center">
+    <section className="about_us_sec pb_100 " style={{ backgroundImage: `url(../../Banne.png)` }}>
+			<div className="container">
+				<div className="row align-items-center">
           <div className="wow slideInLeft">
-					<div class="col-md-8 " >
-						<div class="about_sec_text fvg">
+					<div className="col-md-8 " >
+						<div className="about_sec_text fvg">
             
 							<h1><strong>{data.wpPage.homeAboutSection.heading}</strong> us</h1>
 							<div dangerouslySetInnerHTML={{ __html: data.wpPage.homeAboutSection.shortDescription }} />
@@ -172,44 +154,45 @@ const IndexPage = ({ data }) => {
         
         
         <MeetSlider />
-        <section class="newroom_sec2 pt  pb">
-        <div class="container">
-            <div class="row" data-aos="zoom-out-up" >
-                        <div class="col-lg-12 col-md-12 col-sm-12">
-                            <div class="cret_prt_toptxt">
-                                <h1> <span class="titlecor">{data.wpPage.homeNews.newsHeading}</span></h1>
-                                <p class="font_y">{data.wpPage.homeNews.newsSubheading}</p>
+        <section className="newroom_sec2 pt  pb">
+        <div className="container">
+            <div className="row" data-aos="zoom-out-up" >
+                        <div className="col-lg-12 col-md-12 col-sm-12">
+                            <div className="cret_prt_toptxt">
+                                <h1> <span className="titlecor">{data.wpPage.homeNews.newsHeading}</span></h1>
+                                <p className="font_y">{data.wpPage.homeNews.newsSubheading}</p>
                             </div>
                         </div>
                     </div>
-            <div class="row">
-            {data.allWpNews.edges.map(({ node }) => (
+            <div className="row">
+            {data.allWpNews.edges.map(({ node },index) => (
           <>
-                <div class="col-lg-6 col-md-6 col-sm-12 pb_25" data-aos="flip-right"
-     data-aos-duration="2000">
-                  <div className="wow flipInY" data-wow-duration="1s">
-                    <div class="newsec2_mnbox pb_25">
-                        <img src={node.newsSection.imageArchivePage.sourceUrl} class="img-fluid" alt="" />
-                        <div class="news_imgtxt">
+                <div className="col-lg-6 col-md-6 col-sm-12 pb_25">
+                  {/* <div className="wow flipInY" data-wow-duration="1s"> */}
+                  <ReactWOW animation="slideInUp" delay={`${getAnimationDelay(index)}ms`} duration="2s" >
+                    <div className="newsec2_mnbox pb_25">
+                        <img src={node.newsSection.imageArchivePage.sourceUrl} className="img-fluid" alt="" />
+                        <div className="news_imgtxt">
                         <h4 dangerouslySetInnerHTML={{ __html: node.excerpt.replace(/<p>/, "<h4>").replace(/<\/p>/, "</h4>") }} />
                         </div>
                        
                     </div>
-                    <div class="news_txtbx pb_25">
-                        <div class="newtxtbx pb_20">
-                             <div class="newculbox">
+                    <div className="news_txtbx pb_25">
+                        <div className="newtxtbx pb_20">
+                             <div className="newculbox">
                                  <p><span><Calendar /></span>{node.date}</p>
                              </div>
-                             <div class="newadminbx">
+                             <div className="newadminbx">
                                  <p><span><Adminlogo /></span>Admin</p>
                              </div>
                          </div>
                          <h4 dangerouslySetInnerHTML={{ __html: node.excerpt.replace(/<p>/, "<h4>").replace(/<\/p>/, "</h4>") }} />
-                         <Link to={node.slug} class="readmorebtn" >
+                         <Link to={node.slug} className="readmorebtn" >
                                  Read more<span><ArrowRightIcon /></span>
                              </Link>
                      </div>
-                  </div>
+                     </ReactWOW>
+                  {/* </div> */}
                 </div>
                 </>
         ))}
